395cfc0a95ff0388b12d9ae9c6a6ed74d324fdd9
oycq <490746960@qq.com>
2018-11-29 02:43:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import os
 import numpy as np
 import sys
 
-if len(sys.argv)>=2:
-	print(sys.argv[1])	
+if len(sys.argv)>=2:	
 	VIDEO_DEVICE=int(sys.argv[1])
 cap=cv2.VideoCapture(VIDEO_DEVICE)
 cap.set(cv2.CAP_PROP_FOURCC ,cv2.VideoWriter_fourcc('Y', 'U', 'Y', 'V'))
+if len(sys.argv)>=3:	
+	form=sys.argv[2]
+	cap.set(cv2.CAP_PROP_FOURCC ,cv2.VideoWriter_fourcc(form[0],form[1],form[2],form[3]))
 message=os.popen('v4l2-ctl -d /dev/video%d -P'%VIDEO_DEVICE).read()
 message+=os.popen('v4l2-ctl -d /dev/video%d -V'%VIDEO_DEVICE).read()
+message+=os.popen('v4l2-ctl -d /dev/video%d --list-formats-ext'%VIDEO_DEVICE).read()
 print(message)
 ser=serial.Serial("/dev/ttyUSB0",115200,timeout=0.001)
 while(1):
@@ -26,17 +29,16 @@ while(1):
                 time0=time.time()   
 		ser.write(' ')	
 		_,image1=cap.read()
-                time1=time.time()-time0   
+                time1=(time.time()-time0)*1000   
 		_,image2=cap.read()
-                time2=time.time()-time0    
+                time2=(time.time()-time0)*1000   
 		_,image3=cap.read()
-                time3=time.time()-time0
+                time3=(time.time()-time0)*1000
                 cv2.imshow('before key',image0)
-		cv2.imshow('%.4f'%time1,image1)
-		cv2.imshow('%.4f'%time2,image2)
-		cv2.imshow('%.4f'%time3,image3)
+		cv2.imshow('%.2fms'%time1,image1)
+		cv2.imshow('%.2fms'%time2,image2)
+		cv2.imshow('%.2fms'%time3,image3)
 	if chr(c)=='r':
  		cv2.destroyAllWindows()	
 	if chr(c)=='q':
 		break	
-
